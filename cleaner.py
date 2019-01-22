2a0c561ac6f3c2e8d1a90ff2430d6ad40f0ea7c4
Tom G <griggles@Toms-MacBook-Pro.local>
2019-01-22 11:46:02 -0500

    build images in parallel batches to speed up data retrieval

--- cleaner.py
+++ cleaner.py
@@ -1,9 +1,44 @@
 from PIL import Image
 import urllib.request
+import os 
+import multiprocessing
+import time
+import random
+import pandas as pd
 
-def build_images(df):
+write_lock = multiprocessing.Lock()
+
+
+def create_dataframe():
+    df = pd.read_csv('books.csv',encoding="latin",header=None)
+    df.columns = ['img_idx','img_file','img_link','title','author','cat_id','category']
+    return df
+
+def build_images_linear(df, thread_id=0):   
+    print('thread_id: ', thread_id, ' starting')
+    i_imported = 0
     for index, row in df.iterrows():
-        import_and_resize(row)
+        filename = 'pictures/{}'.format(row["img_file"])
+        if not os.path.isfile(filename):
+            import_and_resize(row)
+            i_imported += 1
+    print('thread_id: ', thread_id, ' finishing')
+    print('thread_id: ', thread_id, 'imported ', i_imported, ' new images')
+
+
+def build_images_parallel(df, num_threads):
+    if __name__ == '__main__':
+       print('Generating images with ', num_threads, ' concurrent processes')
+       num_rows = df.shape[0]
+       batch_size = int(num_rows/num_threads)
+       print('batch size is: ', batch_size)
+       for i in range(num_threads):
+          df_i = df.loc[i*batch_size : min(df.shape[0], (i+1)*batch_size)]
+          print(df_i.head())
+          print(df_i.tail())
+          p = multiprocessing.Process(target=build_images_linear, args=(df_i,i))
+          p.start()
+          
 
 def import_and_resize(df_entry, width=250, height=250):
     filename = 'pictures/{}'.format(df_entry["img_file"])
@@ -15,4 +50,7 @@ def import_and_resize(df_entry, width=250, height=250):
     with open(filename, 'r+b') as f:
         with Image.open(f) as img:
             new_img = img.resize((width,height))
-            new_img.convert("RGB").save(filename, "JPEG", optimize=True)
+            new_img = new_img.convert("RGB")
+            write_lock.acquire()
+            new_img.save(filename, "JPEG", optimize=True)
+            write_lock.release()
